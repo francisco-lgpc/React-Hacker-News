18d8f01770198ac2db922e53a1e9aa7cf554a279
Francisco P Coutinho <francisco.lgpc@gmail.com>
2017-11-23 12:49:00 +0100

    render a list

--- src/App.js
+++ src/App.js
@@ -3,6 +3,26 @@ import logo from './logo.svg';
 import './App.css';
 import { User } from './user.js';
 
+
+const list = [
+  {
+    title: 'React',
+    url: 'https://facebook.github.io/react/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://github.com/reactjs/redux',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
 class App extends Component {
   render() {
     const user = new User("Francisc", "PC");
@@ -11,6 +31,21 @@ class App extends Component {
       <div className="App">
         <h2>{helloWorld}</h2>
         <h2>{user.fullName()}</h2>
+
+        {
+          list.map(item => {
+            return(
+              <ul>
+                { 
+                  Object.keys(item).map((key) => {
+                    return <li>{key + ": " + item[key]}</li>
+                  })
+                }
+              </ul>
+            );
+          })
+        }
+
       </div>
     );
   }
